1d8aaa8a98a205368b38f7a79f1db3f333a9279f
MarleySantoos <135681869+MarleySantoos@users.noreply.github.com>
2024-09-16 07:56:32 -0300

    Add files via upload

---
+++ souls-store/src/js/index.js
@@ -0,0 +1,39 @@
+
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotao();
+		desselecionarPersonagem();
+
+		botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+	});
+});
+
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+}
+
+if (localStorage.getItem("token") == null) {
+	alert("Você precisa estar logado para acessar essa página");
+	window.location.href = "./assets/html/login.html";
+  }
+  
+  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+  
+  const logado = document.querySelector("#logado");
+  logado.innerHTML = `Olá ${userLogado.nome}`;
+  
+  function sair() {
+	localStorage.removeItem("token");
+	localStorage.removeItem("userLogado");
+	window.location.href = "./assets/html/login.html";
+  }
